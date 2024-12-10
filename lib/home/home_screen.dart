0876d5b57798c1d4243b82e44fd1958415d56210
Dell <mohamedelaraby254@gmail.com>
2024-12-11 01:10:40 +0200

    Initial project. Added empty home screen.

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = '0';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
